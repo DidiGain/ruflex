a4b937959a4e34d4b1ef9cb251f984dda7419516
Dinara Gainulina <dignara@yandex.ru>
2022-12-21 14:25:37 +0300

    feat: add Banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import { Banner } from "../components/Banner/Banner";
 
 export default function Home() {
   return (
@@ -14,7 +13,15 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <h1>Ruflex</h1>
+      <main className={styles.main}>
+        <h1>Ruflex</h1>
+        <Banner
+          itemId={""}
+          title="Clifford - the red dog"
+          subtitle="an extremely cute dog"
+          imgUrl="/static/clifford.webp"
+        />
+      </main>
     </>
   );
 }
