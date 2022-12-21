e5fe5d79772d9ff22d9f96ef424be91222647743
Dinara Gainulina <dignara@yandex.ru>
2022-12-21 14:25:27 +0300

    feat: build main layout

---
+++ components/Banner/Banner.props.ts
@@ -0,0 +1,6 @@
+export interface BannerProps {
+  itemId: string;
+  title: string;
+  subtitle: string;
+  imgUrl: string;
+}
---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import Image from "next/image";
+import styles from "./Banner.module.css";
+import { BannerProps } from "./Banner.props";
+
+export const Banner = ({ itemId, title, subtitle, imgUrl }: BannerProps) => {
+  const handleBtnClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div className={styles.bannerContainer}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.leftContent}>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subtitle}>{subtitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.playBtn} onClick={handleBtnClick}>
+              <Image
+                src="/static/icons/play_arrow.svg"
+                alt="Play icon"
+                width="32"
+                height="32"
+              />
+              <span className={styles.playTitle}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className={styles.bannerImg}
+        style={{ backgroundImage: `url(${imgUrl})` }}
+      ></div>
+    </div>
+  );
+};
