1c1d997276f9d04519f41770b0d476a6c31002d7
Dinara Gainulina <dignara@yandex.ru>
2022-12-22 17:30:31 +0300

    feat: add main sign in layout

---
+++ pages/login.tsx
@@ -0,0 +1,67 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Login.module.css";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [userMsg, setUserMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleOnChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setUserMsg("");
+    setEmail(e.target.value);
+  };
+
+  const handleLoginWithEmail = async (e: React.MouseEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <Head>
+        <title>Puffix SignIn</title>
+      </Head>
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <Image
+              src="/static/icons/cinema-icon.svg"
+              alt="Puffix logo"
+              width="50"
+              height="50"
+            />
+            <p className={styles.logoTitle}>Puffix</p>
+          </Link>
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles.emailInput}
+            onChange={handleOnChangeEmail}
+            tabIndex={0}
+          />
+          <p className={styles.userMsg}>{userMsg}</p>
+          <button
+            className={styles.loginBtn}
+            onClick={handleLoginWithEmail}
+            tabIndex={0}
+          >
+            {isLoading ? "Loading..." : "Sign In"}
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
