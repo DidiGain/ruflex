78399505673132ef412cdd213f4f47f077b47b52
Dinara Gainulina <dignara@yandex.ru>
2022-12-22 17:27:15 +0300

    feat: add third domain for yt videos

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ["images.unsplash.com", "i.ytimg.com"] },
+  images: { domains: ["images.unsplash.com", "i.ytimg.com", "yt3.ggpht.com"] },
 };
 
 module.exports = nextConfig;
