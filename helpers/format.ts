1bcc591638c3ce7c245fead608aa4ccbd8771a68
Dinara Gainulina <dignara@yandex.ru>
2022-12-25 19:50:02 +0300

    feat: add formatters to date&number

---
+++ helpers/format.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleString();
+};
+
+export const formatBigNumber = (num: number) => {
+  return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
+};
