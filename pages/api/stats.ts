3cec332397d83d976c9f28d3a9333225fba067bc
Dinara Gainulina <dignara@yandex.ru>
2022-12-30 19:03:57 +0300

    feat: add insert&update stats

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
+import {
+  findVideoStatByUser,
+  insertStats,
+  updateStats,
+} from "../../lib/db/hasura";
+import { verifyToken } from "../../lib/utils";
 
 export default async function stats(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
@@ -9,8 +15,34 @@ export default async function stats(req: NextApiRequest, res: NextApiResponse) {
       const token = req.cookies.token;
       if (!token) res.status(403).send({ msg: "error" });
       else {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-        console.log(decoded);
+        const userId = await verifyToken(token);
+        const videoId = req.query.videoId as string;
+        const { favourited, watched = true } = req.body;
+
+        const findStat = await findVideoStatByUser(token, userId, videoId);
+        const isStatExist = findStat?.length > 0;
+
+        console.log(`is stat: ${isStatExist}`);
+
+        if (isStatExist) {
+          //update
+          const response = await updateStats(token, {
+            userId,
+            videoId,
+            favourited,
+            watched,
+          });
+          res.send({ data: response });
+        } else {
+          //insert
+          const response = await insertStats(token, {
+            userId,
+            videoId,
+            favourited,
+            watched,
+          });
+          res.send({ data: response });
+        }
 
         res.send({ msg: "stats posted" });
       }
