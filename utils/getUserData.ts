4b6b2375bf5c50f840e53077b4c5b63b15ee4fb7
Dinara Gainulina <dignara@yandex.ru>
2023-01-02 10:05:09 +0300

    feat: get token&userId at once

---
+++ utils/getUserData.ts
@@ -0,0 +1,9 @@
+import { GetServerSidePropsContext } from "next/types";
+import { verifyToken } from "../lib/utils";
+
+export const getUserData = async (context: GetServerSidePropsContext) => {
+  const token = context.req ? context.req.cookies?.token : null;
+  const userId = await verifyToken(token as string);
+
+  return { token, userId };
+};
