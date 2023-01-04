8ff49ee1c8427c38ddce6fcfd38df8ab816a9f0c
Dinara Gainulina <dignara@yandex.ru>
2023-01-04 15:12:29 +0300

    refactor: clean code

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { removeCookies } from "../../lib/cookies";
+import { magic } from "../../lib/magic-client";
 import { magicAdmin } from "../../lib/magic-server";
 import { verifyToken } from "../../lib/utils";
 
@@ -7,24 +8,25 @@ export default async function logout(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const token = req.cookies.token;
   try {
-    if (!req.cookies.token) {
+    if (!token) {
       return res.status(401).send({ msg: "User is not logged in" });
-    }
+    } else {
+      const userId = await verifyToken(token);
+      removeCookies(res);
 
-    const token = req.cookies.token;
-    const userId = await verifyToken(token);
-    removeCookies(res);
-
-    try {
-      await magicAdmin.users.logoutByIssuer(userId);
-    } catch (error) {
-      console.error("Error while logging out magic user", error);
+      try {
+        await magicAdmin.users.logoutByIssuer(userId);
+      } catch (error) {
+        console.error("Error while logging out magic user", error);
+      }
     }
 
     res.writeHead(302, { Location: "/login" });
     res.end();
   } catch (error) {
+    console.log({ error });
     res.status(401).send({ msg: "User is not logged in" });
   }
 }
